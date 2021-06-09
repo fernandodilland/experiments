291f94f59760dafb5ed21427bb8cbea00d6686d3
Fernando Dilland Mireles Cisneros <fernandodilland@gmail.com>
2021-06-09 16:16:58 -0500

    Update lenguage

--- Python/ProgramaSimple.py
+++
@@ -1,6 +0,0 @@
-# Programa de prueba para configuración de VS Code (Computadora 2)
-# al Github mediante Github Desktop
-
-print("Programa de prueba")
-variable=input("Ingrese variable cualquiera: ")
-print("La variable es:",variable)
---
+++ Python/Tests/SimpleProgram.py
@@ -0,0 +1,6 @@
+# Test program for VS Code configuration (Computer 2)
+# to Github via Github Desktop
+
+print("Test program")
+variable=input("Enter any variable: ")
+print("The variable is:",variable)
