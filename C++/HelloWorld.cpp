ebe2a3ed0c12332d5bbc72a0b5d46909e4b941c2
Fernando Dilland Mireles Cisneros <fernandodilland@gmail.com>
2021-06-09 17:02:52 -0500

    Creation first C++ code on VS Code and GitHub

--- C++/HelloWorld.cpp
+++ C++/HelloWorld.cpp
@@ -1,8 +1,23 @@
-// First C++ Program
+// Simple C++ program to display "Hello World"
 
 #include <iostream>
+#include <time.h> // For sleep
 
-int main() {
-    std::cout << "Hello World!";
-    return 0;
+using namespace std;
+
+void sleepcp(int milliseconds);
+
+void sleepcp(int milliseconds) // Cross-platform sleep function
+{
+    clock_t time_end;
+    time_end = clock() + milliseconds * CLOCKS_PER_SEC/1000;
+    while (clock() < time_end)
+    {
+    }
+}
+
+int main()
+{
+    cout << "Hello World" << endl;
+    sleepcp(3000); // 3 seconds sleep
 }
