e9c45817ca85e49752b2a3e6bea8a2a2ca436b7e
Fernando Dilland Mireles Cisneros <fernandodilland@gmail.com>
2021-06-09 16:01:05 -0500

    Configuración de prueba

---
+++ Python/ProgramaSimple.py
@@ -0,0 +1,6 @@
+# Programa de prueba para configuración de VS Code (Computadora 2)
+# al Github mediante Github Desktop
+
+print("Programa de prueba")
+variable=input("Ingrese variable cualquiera: ")
+print("La variable es:",variable)
--- Python/Visual.py
+++
@@ -1,5 +0,0 @@
-print("Prueba de código")
-valor = input("Ingrese valor: ")
-print("El valor es:",valor)
-# test
-# test 2
