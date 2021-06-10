0f370ac05b5ebb98af1a1665484689e8a11d54cd
Fernando Dilland Mireles Cisneros <fernandodilland@gmail.com>
2021-06-09 23:23:31 -0500

    Create TestsFunctions.py
    This file was created to see the behavior of 3 test functions

---
+++ Python/Tests/TestsFunctions.py
@@ -0,0 +1,13 @@
+def new_func():
+    set = {1,2,3,4,5,6,7,8,9}
+    return set
+
+def new_func2():
+    tuple = (1,2,3,4,5,6,7,8,9)
+    return tuple
+
+def new_func3():
+    list = [1,2,3,4,5,6,7,8]
+    return list
+
+print(new_func(),new_func2(),new_func3())
