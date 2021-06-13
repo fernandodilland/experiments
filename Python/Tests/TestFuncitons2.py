f0c089e02adf90cecae068e0dc7cedfeb9395a4b
Fernando Dilland Mireles Cisneros <fernandodilland@gmail.com>
2021-06-12 20:22:08 -0500

    Create TestFuncitons2.py

---
+++ Python/Tests/TestFuncitons2.py
@@ -0,0 +1,17 @@
+# Global vs local variable
+
+def func():
+    print("Number on function is:",number)
+    # number=number-1 No workspace
+
+def main():
+    times=20
+    global number
+    number=3
+    print("Number is:",number)
+    while times>0:
+        print("Repeated:",times)
+        times=times-1
+        func()
+
+main()
