9c890cc9893a0da076649a9e17020834d4cafeab
Fernando Dilland Mireles Cisneros <fernandodilland@gmail.com>
2021-06-15 22:12:07 -0500

    Update Exercise_1_Move_Zeros_To_The_Left.py

--- Python/Practice/Exercise_1_Move_Zeros_To_The_Left.py
+++ Python/Practice/Exercise_1_Move_Zeros_To_The_Left.py
@@ -4,8 +4,7 @@
 """Arrays: move zeros to the left
 Given an integer array, move all elements that are 0 to the left while
 maintaining the order of other elements in the array. The array has to be
-modified in-place. Try it yourself before reviewing the solution and
-explanation.
+modified in-place.
 """
 
 def move_zeros(Value): # Function to move
