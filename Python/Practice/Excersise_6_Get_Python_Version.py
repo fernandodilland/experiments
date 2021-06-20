dc372256380e2326526d43bb444f17687fa45c16
Fernando Dilland Mireles Cisneros <fernandodilland@gmail.com>
2021-06-19 20:33:03 -0500

    Create Excersise_6_Get_Python_Version.py

---
+++ Python/Practice/Excersise_6_Get_Python_Version.py
@@ -0,0 +1,8 @@
+# Exercise obtained from the Internet, solved for practice.
+# Programmed by: Fernando Dilland Mireles Cisneros
+
+import sys
+print("Python version:")
+print (sys.version)
+print("Version info:")
+print (sys.version_info)
