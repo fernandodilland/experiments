81339dc05f984b2919bb4a8931625d2848790dda
Fernando Dilland Mireles Cisneros <fernandodilland@gmail.com>
2021-06-19 20:34:47 -0500

    Create Excersise_7_Display_Date_Time.py

---
+++ Python/Practice/Excersise_7_Display_Date_Time.py
@@ -0,0 +1,7 @@
+# Exercise obtained from the Internet, solved for practice.
+# Programmed by: Fernando Dilland Mireles Cisneros
+
+import datetime # Library
+now = datetime.datetime.now() # Save actual date
+print ("Current date and time : ")
+print (now.strftime("%Y-%m-%d %H:%M:%S"))
