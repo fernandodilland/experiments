789322a34b620df78c9a36bd0f9e5b0bce8deecf
Fernando Dilland Mireles Cisneros <fernandodilland@gmail.com>
2021-06-20 17:10:34 -0500

    Update TestClass1.py

--- Python/Tests/TestClass1.py
+++ Python/Tests/TestClass1.py
@@ -1,7 +1,24 @@
 # Experiment programmed for practice by: Fernando Dilland Mireles Cisneros
 
 class Human:
-    def __init__(self, name, age):
+    """
+    init is short for initialization. It is a constructor which gets called
+    when you make an instance of the class and it is not necessary. But usually
+    it our practice to write init method for setting default state of the object.
+    If you are not willing to set any state of the object initially then you
+    don't need to write this method.
+    """
+    def __init__(self, name, age): 
+        """
+        self represents the instance of the class. By using the “self” keyword
+        we can access the attributes and methods of the class in python. It binds
+        the attributes with the given arguments.
+        The reason you need to use self. is because Python does not use the @
+        syntax to refer to instance attributes. Python decided to do methods in
+        a way that makes the instance to which the method belongs be passed
+        automatically, but not received automatically: the first parameter of
+        methods is the instance the method is called on.
+        """
         self.name = name
         self.age = age
 
