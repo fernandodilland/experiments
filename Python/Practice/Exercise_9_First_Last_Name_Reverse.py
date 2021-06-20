faff79a4101a41012483a5ba776c4c019876a018
Fernando Dilland Mireles Cisneros <fernandodilland@gmail.com>
2021-06-19 20:40:47 -0500

    Fix

---
+++ Python/Practice/Exercise_9_First_Last_Name_Reverse.py
@@ -0,0 +1,6 @@
+# Exercise obtained from the Internet, solved for practice.
+# Programmed by: Fernando Dilland Mireles Cisneros
+
+fname = input("Input your First Name: ")
+lname = input("Input your Last Name: ")
+print ("Hello  " + lname + " " + fname)
