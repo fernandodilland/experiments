957458a1572485abe329d113839d19186d575274
Fernando Dilland Mireles Cisneros <fernandodilland@gmail.com>
2021-06-19 21:37:16 -0500

    Update TestDictionarie1.py

--- Python/Tests/TestDictionarie1.py
+++ Python/Tests/TestDictionarie1.py
@@ -1,9 +1,9 @@
 # Experiment programmed for practice by: Fernando Dilland Mireles Cisneros
 
 dictionarie = {
-  "brand": "Nissan",
-  "model": "Tiida",
-  "year": 2007
+    "brand": "Nissan",
+    "model": "Tiida",
+    "year": 2007
 }
 
 print("The brand is:",dictionarie["brand"])
