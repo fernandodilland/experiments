26c82207d0a7d38642cf9d517635fb45098a37c1
Fernando Dilland Mireles Cisneros <fernandodilland@gmail.com>
2021-06-19 20:35:49 -0500

    Create Excersise_8_Radius_Compute_Area.py

---
+++ Python/Practice/Excersise_8_Radius_Compute_Area.py
@@ -0,0 +1,6 @@
+# Exercise obtained from the Internet, solved for practice.
+# Programmed by: Fernando Dilland Mireles Cisneros
+
+from math import pi
+r = float(input ("Input the radius of the circle: "))
+print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
