44616b34dec3ed5dd69776d020ec3f7e2d37ed9e
Fernando Dilland Mireles Cisneros <fernandodilland@gmail.com>
2021-06-20 22:03:28 -0500

    Update List_Set_Tuple_Dictionary.py

--- Python/Tests/List_Set_Tuple_Dictionary.py
+++ Python/Tests/List_Set_Tuple_Dictionary.py
@@ -5,33 +5,36 @@
 # Lists <<<<<<<<<<<
 l = []
 
-# Adding Element into list
+# Adding element into list using append
 print("<<<<< List >>>>>")
 l.append(5)
 l.append(10)
-print("Adding 5 and 10 on list using append:", l)
-l.insert(2, 20)
-l.insert(3, 23)
-print("Adding 20 and 23 on list using insert:", l)
-l.extend([31, 46])
-print("Adding list with 31 and 46 on list using extend:", l)
-l.extend((65, 66))
-print("Adding tuple with 65 and 66 on list using extend:", l)
-l.extend("FER")
-print("Adding string elementos with FER on list using extend:", l)
+print("Adding 5 and 10 in list using append:", l)
 
-# Popping Elements from list
+# Adding element in the middle into set using insert
+l.insert(1, 20)
+l.insert(2, 23)
+print("Adding 20 and 23 (in the middle) in list using insert:", l)
+
+# Adding list into list using extend
+l.extend([31, 46])
+print("Adding list with 31 and 46 in list using extend:", l)
+
+# Adding tuple into list using extend
+l.extend((65, 66))
+print("Adding tuple with 65 and 66 in list using extend:", l)
+
+# Adding string into list using extend
+l.extend("FER")
+print("Adding string elementos with FER in list using extend:", l)
+
+# Popping last element from list
 l.pop()
 print("> Popped the last element from list:", l)
 
-# Force overlapping
-l.insert(2, 85)
-l.insert(3, 73)
-print(">> Overlapping 85 and 73 in list using insert (Pos 2(3) and 3(4) ):", l)
-
-l.extend([999999, 4])
-l.extend([888888888, 5])
-print(">> Adding 93 and 37 in list using extend (Pos 4(5) and 5(6) ):", l)
+# Popping specific element from list
+l.pop(2)
+print("> Popped number 3 (the second one starting from 0) element from list:", l)
 
 print()
 
@@ -44,9 +47,27 @@ s.add(5)
 s.add(10)
 print("Adding 5 and 10 in set:", s)
 
-# Removing element from set
+# Adding set into set
+s2={31,72}
+s.update(s2)
+print("Adding a set (unordered object) with 31 and 72 in set:", s)
+
+# Adding set into set
+s3=[452,926]
+s.update(s3)
+print("Adding contents of another Iterable (unordered) with 452 and 926 in set:", s)
+
+# Removing element from set with remove (if element  does not exist, it raises a KeyError)
 s.remove(5)
-print("Removing 5 from set:", s)
+print("Removing 5 from set with remove:", s)
+
+# Removing element from set with discard (if element  does not exist, it does not raise a KeyError)
+s.discard(72)
+print("Removing 72 from set with discard:", s)
+
+# Removing element from set with pop
+s.pop()
+print("Removing an arbitrary element from set:", s)
 print()
 
 # Tuple <<<<<<<<<<<
@@ -54,7 +75,7 @@ print("<<<<< Tuple >>>>>")
 t = tuple(l)
 
 # Tuples are immutable
-print("Tuple:", t)
+print("Tuple immutable:", t)
 print()
 
 # Dictonary <<<<<<<<<<<
